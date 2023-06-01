dabdb8513429be0c3de6b64b073544b271ee62de
Muhammad0602 <umedjon02.ud@gmail.com>
2023-06-02 01:05:39 +0500

    Add more failing tests for edge cases

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,8 +2,16 @@ require './solver'
 
 describe Solver do
     describe '#factorial' do
-       it 'returns the factorial for a given number' do
+       it 'returns the factorial for the number 4' do
         expect(subject.factorial(4)).to eql(24)
+       end
+
+       it 'returns 1 when the number is 0' do
+        expect(subject.factorial(0)).to eql(1)
+       end
+
+       it 'raises an expection when the number is negative' do
+        expect(subject.factorial(-2)).to eql('raise an error')
        end
     end
 end
