9e3e4142d2c325abe0b2869c75ec1d3fd08cf869
Muhammad0602 <umedjon02.ud@gmail.com>
2023-06-02 01:28:04 +0500

    Refactor code

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,16 +2,22 @@ require './solver'
 
 describe Solver do
     describe '#factorial' do
-       it 'returns the factorial for the number 4' do
+    context 'when the number is positive' do
+       it 'returns 24 when number is 4' do
         expect(subject.factorial(4)).to eql(24)
        end
-
-       it 'returns 1 when the number is 0' do
+    end
+    
+    context 'when the number is 0' do
+       it 'returns 1' do
         expect(subject.factorial(0)).to eql(1)
        end
-
-       it 'raises an expection when the number is negative' do
-        expect{ subject.factorial(-2) }.to raise_error(ArgumentError, 'Input number must be positive')
+    end
+    
+    context 'when the number is negative' do
+       it 'raises an expection' do
+        expect { subject.factorial(-2) }.to raise_error(ArgumentError, 'Input number must be positive')
        end
+    end
     end
 end
