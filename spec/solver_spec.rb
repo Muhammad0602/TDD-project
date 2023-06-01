f5963f37734e5614e9380e25265127163cb08090
Muhammad0602 <umedjon02.ud@gmail.com>
2023-06-02 00:50:35 +0500

    Add failing test for the factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,9 @@
 require './solver'
 
 describe Solver do
-
+    describe '#factorial' do
+       it 'returns the factorial for a given number' do
+        expect(Solver.factorial(4)).to eql(24)
+       end
+    end
 end
